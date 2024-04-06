d1c00ae55145602ff52571813c9af5a2d6e09fff
genemasaka <masakagene@gmail.com>
2024-04-06 23:40:30 +0300

    Created 'Delete user' functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { useRef } from 'react';
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js';
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice.js';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice.js';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -63,6 +63,24 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   }
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,
+      {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -76,7 +94,7 @@ export default function Profile() {
         hover:opacity-95 disabled:opacity-80'>{loading? 'Loading...' : 'Update' }</button>
       </form>
     <div className='flex justify-between mt-5'>
-      <span className='text-red-600 cursor-pointer'>Delete Account</span>
+      <span onClick={handleDeleteUser} className='text-red-600 cursor-pointer'>Delete Account</span>
       <span className='text-red-600 cursor-pointer'>Sign out</span>
     </div>
       <p className='text-red-600 mt-5'>{error ? error : ''}</p>
