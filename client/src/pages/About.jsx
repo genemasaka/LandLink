a34cddf55a4202ea3b07646088540496eb1c989c
genemasaka <masakagene@gmail.com>
2024-04-15 23:14:26 +0300

    Completed 'About Page'

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='mx-auto py-20 px-4 max-w-6xl'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-900'>About LandLink</h1>
+        <p className='mb-4 text-slate-600'>LandLink was born out of a simple idea: connecting people with the land they need to chase their dreams. 
+        You're a seasoned farmer seeking the perfect plot to expand, a budding entrepreneur with a vision for a vineyard, or an adventurer yearning for a slice of nature to call your own, 
+        LandLink is your one-stop shop for leasing land.</p>
+        <p className='mb-4 text-slate-600'>We believe everyone deserves the chance to turn their land-based dreams into reality. Landlords, we make it easy to find responsible tenants and get the most out of your property.</p>
+        <p className='mb-4 text-slate-600'>Land seekers, we put a vast network of possibilities at your fingertips, with user-friendly tools to find the perfect fit.
+        LandLink isn't just about leasing land, it's about fostering connections and empowering potential. Let's grow something great together</p>
     </div>
   )
 }
