8bec55faeb1775e4c1179d606e614bdc9e67e9f7
genemasaka <masakagene@gmail.com>
2024-04-16 09:49:42 +0300

    Adjusted Profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -163,13 +163,7 @@ export default function Profile() {
               key={listing._id}
               className='border rounded-lg p-3 flex justify-between items-center gap-4'
             >
-              {/* <Link to={`/listing/${listing._id}`}>
-                <img
-                  src={listing.imageUrls[0]}
-                  alt='listing cover'
-                  className='h-16 w-16 object-contain'
-                />
-              </Link> */}
+
               <Link
                 className='text-slate-700 font-semibold  hover:underline truncate flex-1'
                 to={`/listing/${listing._id}`}
