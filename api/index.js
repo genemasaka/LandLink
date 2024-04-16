9f365f1555658d865eaf1184fa9c47ab62001cf0
genemasaka <masakagene@gmail.com>
2024-04-16 10:12:49 +0300

    Edited Index.js for deployment

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/listings', listingRouter);
 app.use(express.static(path.join(__dirname, '/clinet/dist')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
